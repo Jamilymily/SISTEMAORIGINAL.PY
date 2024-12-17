d2696c3d899756e88e26cadc6c89ab0f6e86bd86
Jamily Emanuelle <jamilyemanuelle181@gmail.com>
2024-12-17 16:51:01 -0400

    Update GerenciadorRelatorio.py

--- GerenciadorRelatorio.py
+++ GerenciadorRelatorio.py
@@ -1,20 +1,23 @@
 from Relatorio import Relatorio
 
-class GerenciadorRelatorio:
+class GerenciadorRelatorio(Exception):
   def __init__(self):
     self.__relatorio = []
 
   def adicionar_relatorio(self, relatorio):
-    if not isinstance(relatorio, Relatorio):
-      raise TypeError("Instância de Relatório")
-#O raise é uma exceção, garante que apenas ojetos do tipo 
-#Relatorio sejam adicionados a lista.
-    self.__relatorio.append(relatorio)
-
-  def lista_relatorio(self, modo=None):
-    if not self.__relatorio:
-      print("Nenhum Relatório registrado")
-      return
-    for relatorio in self.__relatorio:
-      if modo is None or relatorio.get_tipoRelatorio() == modo:
-        relatorio.exibirRelatorio()
+    try:
+        if not isinstance(relatorio, Relatorio):
+          raise TypeError("Instância de Relatório")
+        self.__relatorio.append(relatorio)
+    except TypeError as e:
+      print(f"Tem um Erro:{e}")
+      
+      def lista_relatorio(self, modo=None):
+        if not self.__relatorio:
+          print("Nenhum Relatório registrado")
+          return
+        for relatorio in self.__relatorio:
+          if modo is None or relatorio.get_tipoRelatorio() == modo:
+            relatorio.exibirRelatorio()
+    finally:
+     print("Execução da lista concluída")
